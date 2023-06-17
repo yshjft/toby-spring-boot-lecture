d075c4ca31a68d93e5c79fbffd04f7a090f3541e
hong yuseok <jericoh@naver.com>
2023-06-18 02:26:54 +0900

    section3: 서블릿 컨테이너 띄우기 + 서블릿 등록

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # toby-spring-boot-lecture
 토비의 스프링 부트 강의 코드 레파지토리
+
+## SECTION 3 독립 실행형 서블릿 애플리케이션
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,13 +1,38 @@
 package tobyspring.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
 
-@SpringBootApplication
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HellobootApplication.class, args);
-	}
+		// 임베디드 Tomcat(서블릿 컨테이너)을 직접 띄워보자!
+		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 
+		// WebServer : 여러 웹서버를 사용할 수 있도록 추상화함
+		// ServletContextInitializer를 통해 컨테이너에 서블릿을 등록하자!
+		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+			// servlet을 추가
+			// HttpServlet: 일종의 adapter 클래스
+			// addMapping을 통해 요청이 서블릿과 매핑될 수 있도록 한다.
+			servletContext.addServlet("hello", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					// 응답을 만든다.
+					resp.setStatus(200);
+					resp.setHeader("Content-Type", "text/plain");
+					resp.getWriter().print("Hello Servlet");
+				}
+			}).addMapping("/hello");
+		});
+
+		// Tomcat Servlet Container 동작
+		webServer.start();
+	}
 }
