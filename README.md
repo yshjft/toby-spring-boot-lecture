719941b7453e56e808529e3f5243cd65f59fec67
hong yuseok <jericoh@naver.com>
2023-07-06 01:41:07 +0900

    section4 - 자바코드 구성 정보 사용

--- README.md
+++ README.md
@@ -17,6 +17,7 @@
 * 스프링 컨테이너는 POJO와 구성 정보를 런타임에 조합해서 최종 애플리케이션을 만들어 낸다.
 * POJO : Business Object, 다른 외부 클래스나 라이브러리에 의존성이 전혀 없다.
 * 서블릿 컨테이너는 HelloController라는 타입의 오브젝트(Bean)가 어떻게 만들어 졌는지 신경쓰지 않는다.
+  * 스프링 컨테이너는 서블릿 컨테이너와 다르게 직접 객체를 만들어서 넣어줄 필요가 없다.
 
 ### 의존 오브젝트 추가
 * 기존 서블 컨테이너만 있는 구조에서 스프링 컨테이너가 추가되어서 가지는 이점은 도대체 무엇일까?
@@ -32,7 +33,47 @@
 * **어셈블러**
   * 두 개의 오브젝트가 동적으로 의존 관계를 가지는 것을 도와주는 제 3의 존재
   * 스프링 컨테이너가 어셈블러의 역할을 한다.
+* bean 등록 (DI는 스프링 컨테이너가 알아서 잘 해줄겁니다 ~^^~)
+```java
+applicationContext.registerBean(HelloController.class);
+applicationContext.registerBean(SimpleHelloService.class);
+```
 
-### DispatcherServlet
+### DispatcherServlet으로 전환
 * DispatcherServlet은 매핑 및 바인딩 작업을 직접 구현할 필요가 없게 만들어준다.
+* 서블릿 컨테이너가 초기화 되는 과정을 스프링 컨테이너가 초기화 되는 과정 중에 일어나도록 만들자. onRefresh가 호출되는 시점에 서블릿 컨테이너를 초기화힌디.
+```java
+GenericWebApplicationContext applicationContext = new GenericWebApplicationContext(){
+    @Override
+    protected void onRefresh() {
+        super.onRefresh();
+        
+        TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("dispatcherServlet",new DispatcherServlet(this))
+                    .addMapping("/*");
+        });
+        
+        webServer.start();
+    }
+};
+```
 
+### 자바 코드 구성 정보 사용
+* 왜 팩토리 메서드를 사용하나?
+  * 팩토리 메서드 방식 = ```@Configuration``` + ```@Bean```
+  * 왜 구지 bean 객체를 직접 생성해서 주입하려고 하나.. → bean을 만들고 초기화하는 작업이 복잡한 경우가 있다 → 자바 코드로 간결하게 만들자
+* Configuration 클래스
+  * 구성 정보 클래스
+  * 쉽게 말해 빈 팩토리 메서드가 있는 클래스
+  * 빈 구성 정보가 담겨 있는 클래스를 의미한다.
+* GenericWebApplicationContext는 자바 코드로 만든 configuration 정보를 읽을 수가 없다. 따라서 AnnotationConfigWebApplicationContext로 교체한다.
+  * applicationContext에 사용하려는 클래스를 모두 등록하는 대신 Configuration 클래스를 등록한다.
+    ```java
+    applicationContext.register(HellobootApplication.class);
+    ```
+* ```@Configuration```이 붙은 클래스가 ApplicationContext에 처음으로 등록된다.
+
+
+### @Component 스캔
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -2,42 +2,41 @@ package tobyspring.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+@Configuration
 public class HellobootApplication {
+	@Bean
+	public HelloController helloController(HelloService helloService) {
+		return new HelloController(helloService);
+	}
+	@Bean
+	public HelloService helloService() {
+		return new SimpleHelloService();
+	}
 
 	public static void main(String[] args) {
-		// 스프링 컨테이너 생성
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext(){
-			// 서블릿 컨테이너가 초기화 되는 과정을 스프링 컨테이너가 초기화 되는 과정 중에 일어나도록 만들자. onRefresh가 호출되는 시점에 서블릿 컨테이너를 초기화힌디.
+		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext(){
 			@Override
 			protected void onRefresh() {
 				super.onRefresh();
 
-				// 임베디드 Tomcat(서블릿 컨테이너)을 직접 띄워보자!
 				TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 
-				// WebServer : 여러 웹서버를 사용할 수 있도록 추상화함
-				// ServletContextInitializer를 통해 컨테이너에 서블릿을 등록하자!
 				WebServer webServer = serverFactory.getWebServer(servletContext -> {
-					// servlet을 추가
-					// HttpServlet: 일종의 adapter 클래스
-					// addMapping을 통해 요청이 서블릿과 매핑될 수 있도록 한다.
-					servletContext.addServlet("dispatcherServlet",
-										new DispatcherServlet(this))
+					servletContext.addServlet("dispatcherServlet", new DispatcherServlet(this))
 							.addMapping("/*");
 				});
 
-				// Tomcat Servlet Container 동작
 				webServer.start();
 			}
 		};
 
-		// bean 등록 (DI는 스프링 컨테이너가 알아서 잘 해줄겁니다 ~^^~)
-		applicationContext.registerBean(HelloController.class);
-		applicationContext.registerBean(SimpleHelloService.class);
-		// 스프링 컨테이너 초기화 작업
+		applicationContext.register(HellobootApplication.class);
+
 		applicationContext.refresh();
 	}
 }
